a80b192e01d7ed1ec95145137e5a45f9d4523226
Daniel Ganchev <dani.ganchev1989@gmail.com>
2023-09-27 11:42:28 +0300

    new

---
+++ mobilele/src/main/java/org/softuni/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.softuni.repository;
+
+import org.softuni.model.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
--- mobilele/src/main/java/org/softuni/service/UserServiceImpl.java
+++ mobilele/src/main/java/org/softuni/service/UserServiceImpl.java
@@ -1,15 +1,30 @@
 package org.softuni.service;
 
 import org.softuni.model.dto.UserRegistrationDto;
+import org.softuni.model.entities.UserEntity;
+import org.softuni.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+
+
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+
 
     @Override
     public void register(UserRegistrationDto userRegistrationDto) {
-
+        userRepository.save(map(userRegistrationDto));
 
 
     }
@@ -18,4 +33,13 @@ public class UserServiceImpl implements UserService {
     public void login(String email, String password) {
 
     }
-}
+
+    private  UserEntity map(UserRegistrationDto userRegistrationDto){
+            return new UserEntity()
+                    .setActive(true)
+                    .setFirstName(userRegistrationDto.firstName())
+                    .setLastName(userRegistrationDto.lastName())
+                    .setEmail(userRegistrationDto.email())
+                    .setPassword(passwordEncoder.encode(userRegistrationDto.password()));
+        }
+    }
